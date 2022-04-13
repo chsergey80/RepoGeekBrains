81f4c3a8ff995778b12155f9e1691e4a91105858
chsergey80 <chsergey@yandex.ru>
2022-04-13 23:41:44 +0300

    Update 13.04.2022 v.4

--- Basics-Python/1.py
+++ Basics-Python/1.py
@@ -1,5 +1,4 @@
-from requests import get, utils
-response = get('http://geekbrains.ru')
-encodings = utils.get_encoding_from_headers(response.headers)
-content = response.content.decode(encoding=encodings)
-print(content)
+from numpy import pi, sin, cos
+
+print(pi)
+print(sin(6))
