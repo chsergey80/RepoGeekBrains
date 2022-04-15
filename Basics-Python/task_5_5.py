68bf60a052604bf71794344b9f1afb2e49f3e601
chsergey80 <chsergey@yandex.ru>
2022-04-15 23:36:34 +0300

    Update 15.04.2022 v.1

--- Basics-Python/task_5_5.py
+++ Basics-Python/task_5_5.py
@@ -7,37 +7,26 @@ result = [23, 1, 3, 10, 4, 11]
 Подсказка: напишите сначала решение «в лоб». Потом подумайте об оптимизации.
 """
 
-import time  # use for test
-import sys  # use for test
+import time
+import sys
 
 
 def my_set(*argv):
-    """ return unique elemts of argv """
     answ = set()
-
     for elem in argv:
         if not elem in answ:
             answ.add(elem)
         else:
             answ.remove(elem)
-
-    return [x for x in argv if x in answ]  # best
-    # return [ x for x in argv if argv.count(x) == 1 ] # worst
-    # return [ x for i, x in enumerate(argv) if not x in [*argv[:i], *argv[i+1:]] ] # bad
+    return [x for x in argv if x in answ]
 
 
 if __name__ == "__main__":
-
     src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
     result = [23, 1, 3, 10, 4, 11]
-
     t = time.perf_counter()
     r = my_set(*src)
-
     print("mem: ", sys.getsizeof(r))
     print("time: ", time.perf_counter() - t)
-
     print(r == result)
     print(r)
-
-    print(test_set(*src))
